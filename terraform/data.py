d308007d465adb41d4cbb58d7626ab3ed36c126a
Josephson Junior <josephson-junior.razafindratsiza@eleve.ensai.fr>
2025-05-03 17:40:33 +0200

    data db

--- terraform/data.py
+++ terraform/data.py
@@ -1,57 +1,55 @@
-# postgram/terraform/data.py
-
 data = [
     {
-        "user": "Deku",  # <--- Sans préfixe
-        "id": "60888533-cc26-4e59-93ab-6964850ed447", # <--- Sans préfixe
+        "user": "Deku",
+        "id": "60888533-cc26-4e59-93ab-6964850ed447",
         "title": "All Might",
         "body": "Le symbole de la paix !",
         "image": "Deku/60888533-cc26-4e59-93ab-6964850ed447/ec3cd817-4655-4e30-9f90-860f716bc7b1all might.jpeg",
         "labels": []
     },
     {
-        "user": "Link", # <--- Sans préfixe
-        "id": "915d2d13-cea2-4b80-8c5b-e90295b16876", # <--- Sans préfixe
+        "user": "Link", 
+        "id": "915d2d13-cea2-4b80-8c5b-e90295b16876",
         "title": "Master Sword",
         "body": "L'épée légendaire.",
         "image": "Link/915d2d13-cea2-4b80-8c5b-e90295b16876/1ce475b3-af6f-4c3b-b31c-d3acc2379ebcmaster sword.jpeg",
         "labels": []
     },
     {
-        "user": "Link", # <--- Sans préfixe
-        "id": "28d5598c-c699-4c9b-ab7b-0a0d921b1583", # <--- Sans préfixe
+        "user": "Link",
+        "id": "28d5598c-c699-4c9b-ab7b-0a0d921b1583",
         "title": "Attention Lynel !",
         "body": "Rencontre dangereuse.",
         "image": "Link/28d5598c-c699-4c9b-ab7b-0a0d921b1583/fb85dfdb-c0e9-4fd5-b2da-832610e00303lynel.jpg",
         "labels": []
     },
     {
-        "user": "Setsuna", # <--- Sans préfixe
-        "id": "cf536095-f4b8-42ce-a6e9-b0be7b9e1566", # <--- Sans préfixe
+        "user": "Setsuna"
+        "id": "cf536095-f4b8-42ce-a6e9-b0be7b9e1566",
         "title": "00 Raiser Trans-AM",
         "body": "Vitesse maximale !",
         "image": "Setsuna/cf536095-f4b8-42ce-a6e9-b0be7b9e1566/a3df4756-399f-4517-bc1e-e387c1352df6trans AM 00.jpg",
         "labels": []
     },
     {
-        "user": "Setsuna", # <--- Sans préfixe
-        "id": "48d6fb25-0685-4236-bf21-477aa7e66023", # <--- Sans préfixe
+        "user": "Setsuna", 
+        "id": "48d6fb25-0685-4236-bf21-477aa7e66023",
         "title": "Gundam 00 Qan[T]",
         "body": "Prêt pour le dialogue.",
         "image": "Setsuna/48d6fb25-0685-4236-bf21-477aa7e66023/f9547e91-431e-425a-894f-e55224c7ef92gundam quanT.jpg",
         "labels": []
     },
     {
-        "user": "Anya", # <--- Sans préfixe
-        "id": "dfe03fe8-8eb0-4060-ab44-3be4ab9db2d5", # <--- Sans préfixe
+        "user": "Anya",
+        "id": "dfe03fe8-8eb0-4060-ab44-3be4ab9db2d5",
         "title": "Mama Yor",
         "body": "Elle est belle.",
         "image": "Anya/dfe03fe8-8eb0-4060-ab44-3be4ab9db2d5/103dbabe-cdbc-4477-98c6-596cd05329f0yor-forger.jpg",
         "labels": []
     },
     {
-        "user": "Anya", # <--- Sans préfixe
-        "id": "e785e61b-91a9-41fc-ad07-eebbef55cb2b", # <--- Sans préfixe
+        "user": "Anya",
+        "id": "e785e61b-91a9-41fc-ad07-eebbef55cb2b",
         "title": "Mama Yor (mode assassin)",
         "body": "Elle fait peur des fois...",
         "image": "Anya/e785e61b-91a9-41fc-ad07-eebbef55cb2b/990e5717-a1c1-4caa-8beb-5ae28c2ef462yor asssassin.jpg",
